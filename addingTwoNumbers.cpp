560cf2980abcbd19285541950ddd959b193ff0ca
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-09 17:47:54 +0300

    Add files via upload

---
+++ addingTwoNumbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, sum;
+	cout<< "Sum of two integers input by the user:\n";
+	cout<< ".........................................\n";
+	cout<< "Input first number:\n";
+	cin>> a;
+	cout<< "input second number:\n";
+	cin>> b;
+	sum= a+b;
+	cout<< "the sum of the two numbers is: " << sum;
+	return 0;
+}
+
